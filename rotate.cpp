5bb642cd7788f1fd896073dac819309933d01d19
Harsh Somya <97454591+harshsomya004@users.noreply.github.com>
2022-01-11 16:13:14 +0530

    Add files via upload

---
+++ rotate.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h> 
+#include<vector>
+using namespace std;
+
+void print_mat(vector<vector<int>> arr, int n)
+{
+    for(int i=0;i<n;i++)
+    { 
+        for(int j=0;j<n;j++)
+        {
+            cout <<arr[i][j]<<" ";   
+        }  
+        cout<< endl; 
+    }
+}
+
+
+vector<vector<int>> rotate_mat(vector<vector<int>> arr, int n)
+{
+    vector<vector<int>> res = arr;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            res[j][n-1-i] = arr[i][j];
+        }
+    }
+    return res;     
+}
+
+int main()
+{ 
+    int n,num ;
+    vector<vector<int>> res;
+    cin>> n;
+    for(int i=0;i<n;i++)
+    { 
+        vector<int>temp;
+        res.push_back(temp);
+        for(int j=0;j<n;j++)
+        {
+            cin >>num;
+            res[i].push_back(num);   
+        }
+    }
+    
+    auto mat90 = rotate_mat(res,n);
+    print_mat(mat90,n);
+    cout<<endl;
+    auto mat180 = rotate_mat(mat90,n);
+    print_mat(mat180,n);
+    return 0;
+}
+/* test cases
+Sample Input
+2 3 4 7 6 
+
+Sample Output
+7 3 
+6 4 
+
+6 7 
+4 3
+
+Sample Input:
+2
+1 2
+3 4
+
+Sample Output:
+3 1
+4 2
+
+4 3
+2 1
+*/
